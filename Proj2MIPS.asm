df26574c35eddac408e41f83c72868a380fbd313
jasmoncooley <jasmoncooley89@gmail.com>
2018-11-06 19:41:10 -0500

    Learning how to set up an array

--- Proj2MIPS.asm
+++ Proj2MIPS.asm
@@ -16,3 +16,21 @@
 	la $a0, str
 	syscall
   
+  	addi $t0, $zero, 0
+  	while:
+  	beq $t0, 40, exit
+  	lw $t6, myArray($t0)
+  	addi $t0, $t0, 4
+  	li $v0, 1
+  	move $a0, $t6
+  	syscall
+  
+  	li $v0,4
+  	la $a0, newLine
+  	syscall
+  
+  	j while
+  
+ exit:
+ 	li $v0,10
+ 	syscall
