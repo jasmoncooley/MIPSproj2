de594bae6459c7842e223da7b297cd7cce000fcb
jasmoncooley <jasmoncooley89@gmail.com>
2018-11-12 22:35:22 -0500

    created main function
    read in user input
    set a1 as 'A' to start branching or comparing values in addresses

--- Proj2MIPS.asm
+++ Proj2MIPS.asm
@@ -4,3 +4,12 @@
 	tooLongMessage: .asciiz "too long"
 	notInRange: .asciiz "not in range"
 
+.text
+	main:
+	
+		li $v0, 8 #read in integer
+		la $a0, str
+		li $a1, 65 #this is A
+		syscall
+
+		loop:
