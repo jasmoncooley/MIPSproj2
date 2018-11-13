f7fc79847c9fc37a60c9a74b317fca72196fe9a7
jasmoncooley <jasmoncooley89@gmail.com>
2018-11-12 22:33:40 -0500

    Storing message for branching
    Redoing my code to include branching

--- Proj2MIPS.asm
+++ Proj2MIPS.asm
@@ -1,36 +1,6 @@
 .data
-	myArray: .word 5:10
+	str: .space 10
 	newLine: .asciiz "\n"
-	str:
-		.space 16
+	tooLongMessage: .asciiz "too long"
+	notInRange: .asciiz "not in range"
 
-.text
-	main: 
-	
-	li $v0, 8
-	la $a0, str
-	li $a1, 16
-	syscall
-	
-	li $v0, 4
-	la $a0, str
-	syscall
-  
-  	addi $t0, $zero, 0
-  	while:
-  	beq $t0, 40, exit
-  	lw $t6, myArray($t0)
-  	addi $t0, $t0, 4
-  	li $v0, 1
-  	move $a0, $t6
-  	syscall
-  
-  	li $v0,4
-  	la $a0, newLine
-  	syscall
-  
-  	j while
-  
- exit:
- 	li $v0,10
- 	syscall
