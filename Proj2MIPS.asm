1b0d3806338d900801647969b02d15b7197985da
jasmoncooley <jasmon.cooley@bison.howard.edu>
2018-11-17 00:19:03 -0500

    make sure the input is in range

--- Proj2MIPS.asm
+++ Proj2MIPS.asm
@@ -1,48 +1,84 @@
 .data
-	user: .space 10
 	newLine: .asciiz "\n"
 	emptyMessage: .asciiz "nothing is here"
 	tooLongMessage: .asciiz "too long"
 	notInRange: .asciiz "not in range of base 32"
+	user: .space 50
 
 .text
-	main:
-	
+    main:
 		li $v0, 8 #read in integer
 		la $a0, user
-		addi $t1, $t1, 10  #Load line feed char
+		li $a1, 51
 		syscall
 
 		LengthSpaceLoop:
-			lbu $t1, 0($a0) #load the character into t1
-			beqz $t1, empty
-			beq $t2, $t1, done   #End when line feed is detected
-			beq $t1,' ',skipSpace #if it is space then go to the next value
-            		addi $t0, $t0, 1 #count the amount of characters
-   			addi $a0, $a0, 1
-       			j loop
-		exit:
-    			j end
-    		tooLong:
-			li $v0, 4
-			la $a0, tooLongMessage
-			syscall
-			j end
+		li $t8, 32  #Load space char
+		li $t1, 10 #new line
+		li $t5, 4
+		lb $t2, 0($a0) #load the character into t1
+		beq $t8, $t2, skipSpace   #End when line feed is detected
+		#move $t4, $a0
+		#beq $t1,$t2, lengthCheck #if it is space then go to the next value
+		beqz $t2, empty
+            	addi $t0, $t0, 1 #count the amount of characters
+   		addi $a0, $a0, 1
+   		beq $t0, $t5, length
+       		j LengthSpaceLoop
+       		
 		empty:
-			li $v0, 4
-			la $a0, emptyMessage
-			syscall
-			j end
+		li $v0, 4
+		la $a0, emptyMessage
+		syscall
+		j end
+		
+		length:
+		beq $t1, $t2, lengthCheck
+		#move $t4, $a0 #store content of a0
+		slti $t3, $t0, 5      #Check that count is less than 5
+		beqz $t3, tooLong #Branch to length error if length is 5 or more
+		#move $a0, $t4
+		j checkString
+		
+		lengthCheck:
+		beqz $t0, emptyMessage   #Branch to null error if length is 0
+		#move $a0, $t4
+		j length
+		
 		skipSpace:
-			addi $a0,$a0,1
-			sub $t0, $t0, 1 #if we skip a space, we want to subtract that character frorm our length
-			j LengthSpaceLoop
+		addi $a0,$a0,1
+		#sub $t0, $t0, 1 #if we skip a space, we want to subtract that character frorm our length
+		move $t4, $a0
+		j LengthSpaceLoop
+		
 		range1:
-			li $v0, 4
-			la $a0, tooLongMessage
-			syscall	
-		lessthan:
-			ble $t1,$a1, notInRange
-     end:
-     li $v0, 10			
-     syscall
+		li $v0, 4
+		la $a0, notInRange
+		syscall	
+		tooLong:
+		li $v0, 4
+		la $a0, tooLongMessage
+		syscall
+		j end
+			
+		checkString:
+		lb $t5, 0($a0)
+		beqz $t5, convert  #End if null character is reached
+		beq $t5, $t1, convert  #End loop if NL is detected
+		slti $t6, $t5, 48    #Check if less than 0 
+		bne $t6, $zero, baseError
+		slti $t6, $t5, 58    #Check if less than 9 
+		bne $t6, $zero, Increment
+		slti $t6, $t5, 65    #Check if less than A 
+		bne $t6, $zero, baseError
+		slti $t6, $t5, 87    #Check if less than W
+		bne $t6, $zero, Increment
+		slti $t6, $t5, 97    #Check if less a
+		bne $t6, $zero, baseError
+		slti $t6, $t5, 119   #Check if less than w
+		bne $t6, $zero, Increment
+		bgt $t5, 118, baseError   #Check if is greater than v
+
+
+
+		
