541af69863cbcafd4910a2bae128854964a26076
jasmoncooley <jasmon.cooley@bison.howard.edu>
2018-11-16 19:44:10 -0500

    Adds small changes

--- Proj2MIPS.asm
+++ Proj2MIPS.asm
@@ -1,26 +1,25 @@
 .data
-	str: .space 10
+	user: .space 10
 	newLine: .asciiz "\n"
+	emptyMessage: .asciiz "nothing is here"
 	tooLongMessage: .asciiz "too long"
-	notInRange: .asciiz "not in range"
+	notInRange: .asciiz "not in range of base 32"
 
 .text
 	main:
 	
 		li $v0, 8 #read in integer
-		la $a0, str
+		la $a0, user
 		li $a1, 65 #this is A
 		syscall
 
-		loop:
+		LengthSpaceLoop:
 			lbu $t1, 0($a0) #load the character into t1
 			bge $t1, $a1, tooLong
 			beqz $t1, exit
-			beq $t1,' ',loop #if it is space, the go to the top of the loop
-            		
-            		addi $t0, $t0, 1
+			beq $t1,' ',skipSpace #if it is space then go to the next value
+            		#addi $t0, $t0, 1
    			addi $a0, $a0, 1
-            
        			j loop
 		exit:
      			li $v0, 11        # Print chars
@@ -31,4 +30,16 @@
 			li $v0, 4
 			la $a0, tooLongMessage
 			syscall
-    	
+			j end
+		skipSpace:
+			addi $a0,$a0,1
+			j LengthSpaceLoop
+		range1:
+			li $v0, 4
+			la $a0, tooLongMessage
+			syscall	
+		lessthan:
+			ble $t1,$a1, notInRange
+     end:
+     li $v0, 10			
+     syscall
